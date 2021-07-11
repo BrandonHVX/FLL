602ad384288c41a3315a80768867bb2cacb06265
Brandon <brandongines@gmail.com>
2021-07-11 12:56:29 -0400

    first commit-1

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/favicon.ico',
+  manifestIcon: 'src/assets/img/icons/star.png',
   pathPrefix: `/gatsby-starter-newage/`, // This path is subpath of your hosting https://domain/portfolio
   // social
   socialLinks: [
